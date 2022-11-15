a21595aec9e55f023a0346ea2a03291f4f6b2355
nwilki <72949393+Cr1ms0nC0de@users.noreply.github.com>
2022-11-15 11:59:37 -0600

    Finished Apple Pie

--- Apple Pie/Apple Pie/ViewController.swift
+++ Apple Pie/Apple Pie/ViewController.swift
@@ -9,11 +9,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var treeImageView: UIImageView!
+    var listOfWords = ["pufferfish", "dog", "cat", "walrus", "seal", "swordfish", "crab", "dolphin"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
     }
-
-
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var currentGame: Game!
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        for i in letterButtons{
+            i.addTarget(self, action: #selector(letterButtonPressed(_:)), for: .touchUpInside)
+        }
+        newRound()
+    }
+    func newRound(){
+        if !listOfWords.isEmpty{
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord,
+            incorrectMovesRemaining: incorrectMovesAllowed,
+            guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        }
+        else{
+            enableLetterButtons(false)
+        }
+    }
+    func enableLetterButtons(_ enable: Bool){
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    func updateUI(){
+        var letters = [String]()
+        for letter in currentGame.formattedWord{
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    @IBAction func letterButtonPressed(_ sender: UIButton){
+        sender.isEnabled = false
+        let letterString = (sender.titleLabel?.text)
+        let letter = Character(letterString!.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    func updateGameState(){
+        if currentGame.incorrectMovesRemaining == 0{
+            totalLosses += 1
+        }
+        else if currentGame.word == currentGame.formattedWord{
+            totalWins += 1
+        }
+        else {
+            updateUI()
+        }
+    }
 }
 
