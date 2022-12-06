d1abc5f337350adf8b98a9a49c5737d63d952a08
nwilki <72949393+Cr1ms0nC0de@users.noreply.github.com>
2022-12-06 12:14:59 -0600

    Still not done with Tip Calc

---
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Wilkins, Nolan W on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var customPercentage: UIView!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var totalPrice: UILabel!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var tipPercentage: UISegmentedControl!
+    @IBOutlet weak var billAmount: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
