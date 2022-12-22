fa2f311001aa3f3abcb9483622199e56d9fd0e8d
nwilki <72949393+Cr1ms0nC0de@users.noreply.github.com>
2022-12-22 11:22:48 -0600

    Finished Add One

--- AddOne/AddOne/ViewController.swift
+++ AddOne/AddOne/ViewController.swift
@@ -13,33 +13,85 @@ class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var inputField: UITextField!
+    var timer = Timer()
+    var timer2 = Timer()
+    var startTimer = false
+    var seconds = 10
+    var counter = 10
     
+    var score: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background.jpg")!)
         numberLabel.text = "\(Int.random(in: 1000...9999))"
+        
     }
+    
+    
     @IBAction func inputFieldChanged(_ sender: UITextField) {
+        if(startTimer == false){
+            timer = Timer.scheduledTimer(timeInterval: TimeInterval(seconds), target: self, selector: #selector(timerFunction), userInfo: nil, repeats: false)
+            timer2 = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        }
+        startTimer = true
         guard let num = Int(inputField.text!) else{
+            inputField.text = ""
             return
         }
         guard inputField.text!.count == 4 else{
             return
         }
-        guard var requiredNum = Int(numberLabel.text!) else{
-            return
-        }
         let input = numberLabel.text
-        for i in 0...input!.count{
+        var output: String = ""
+        var outputChar: Int = 0
+        for i in 0...input!.count-1{
             let char = input![input!.index(input!.startIndex, offsetBy: i)]
-            guard Int(String(char)) == 9 else{
-                return
+            outputChar = Int(String(char))!
+            if Int(String(char)) == 9{
+                outputChar = 0
             }
+            else{
+                outputChar += 1
+            }
+            output.append(String(outputChar))
         }
-            
-        if(num == Int(numberLabel.text!)){
-            
+        if(num == Int(output)){
+            numberLabel.text = "\(Int.random(in: 1000...9999))"
+            score += 1
+            scoreLabel.text = String(score)
+            inputField.text = ""
+        }
+        else{
+            numberLabel.text = "\(Int.random(in: 1000...9999))"
+            score -= 1
+            scoreLabel.text = String(score)
+            inputField.text = ""
+        }
+    }
+    
+    @objc func timerFunction(){
+        timer.invalidate()
+        
+        let dialogMessage = UIAlertController(title: "Game Over!", message: "Score: \(score)", preferredStyle: .alert)
+        let restart = UIAlertAction(title: "Restart", style: .default, handler: { [self] (action) -> Void in
+            numberLabel.text = "\(Int.random(in: 1000...9999))"
+            score = 0;
+            scoreLabel.text = String(score)
+            inputField.text = ""
+            timeLabel.text = "\(seconds)"
+            counter = seconds
+            startTimer = false
+          })
+        dialogMessage.addAction(restart)
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    @objc func updateCounter(){
+        if counter > 0 && startTimer {
+            counter -= 1
+            timeLabel.text = "\(counter)"
+        }
+        if counter == 0{
+            timer2.invalidate()
         }
     }
 }
-
