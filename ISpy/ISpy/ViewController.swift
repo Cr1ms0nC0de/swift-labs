f4fe5ad64b6853f7ca95ec73672e42e25c0e4910
nwilki <72949393+Cr1ms0nC0de@users.noreply.github.com>
2023-04-19 12:15:04 -0500

    Finished ISpy

--- ISpy/ISpy/ViewController.swift
+++ ISpy/ISpy/ViewController.swift
@@ -7,20 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate{
 
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pufferfish: UIImageView!
+    @IBOutlet weak var mainView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        pufferfish.frame.origin = CGPoint(x: 1, y: 1)
+        pufferfish.frame.origin = CGPoint(x: Int.random(in: 0...(1200 - Int(pufferfish.frame.width))), y: Int.random(in: 0...(1200 - Int(pufferfish.frame.height))))
+        print(pufferfish.frame.origin)
         let tapGR = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
         pufferfish.addGestureRecognizer(tapGR)
         pufferfish.isUserInteractionEnabled = true
+        setupScrollView()
     }
     
     @objc func imageTapped(sender: UITapGestureRecognizer) {
-        print("Pufferfish")
+        pufferfish.frame.origin = CGPoint(x: Int.random(in: 0...(1200 - Int(pufferfish.frame.width))), y: Int.random(in: 0...(1200 - Int(pufferfish.frame.height))))
+        print(pufferfish.frame.origin)
+    }
+    
+    func setupScrollView(){
+        scrollView.delegate = self
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return mainView
     }
 }
 
